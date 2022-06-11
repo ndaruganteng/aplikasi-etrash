59d1cfe52f11308664985251711fb1282af552f0
M Ndaru Sabitturahman <muhamadndaru2001@gmail.com>
2022-06-11 15:27:56 +0700

    apk

---
+++ app/src/main/java/com/example/etrash/MainActivity_menu.kt
@@ -0,0 +1,15 @@
+package com.example.etrash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity_menu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_menu)
+    }
+
+    fun btnScane_click(view: android.view.View) {}
+    fun btn_click(view: android.view.View) {}
+    fun btnaboutlick(view: android.view.View) {}
+}
